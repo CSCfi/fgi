3246700f9ba1e91012b9a96c849c9d8720e3b548
Kalle Happonen <kalle.happonen@csc.fi>
2011-12-14 14:31:00 +0000

    minor cuda fix
    git-svn-id: svn+ssh://svn.csc.fi/svn/fgi@4026 2bf07f67-1f50-48e5-81db-618ea08fc75d

--- rpm/cluster-tools/etc/cluster/scripts/cuda.sh
+++ rpm/cluster-tools/etc/cluster/scripts/cuda.sh
@@ -6,9 +6,9 @@ if [ ! -z "$NVIDIACHECK" ]; then
  echo "chmod 0666 /dev/nvidia*" >> /etc/rc.d/rc.local
  GT520CHECK=`lspci -d 10de:1040`
   if [ -z ! "$GT520CHECK" ]; then
-  	yum -y install nvidia-cuda-lite 
+  	yum -y install nvidia-ganglia-lite 
   else 
-	yum -y install nvidia-cuda
+	yum -y install nvidia-ganglia
   fi
 fi
 
