f25636ed4456f189506ad9b9bd52bb213f5fb20c
Ulf Tigerstedt <ulf.tigerstedt@csc.fi>
2011-12-14 13:37:14 +0000

    Fixed the if-clause
    git-svn-id: svn+ssh://svn.csc.fi/svn/fgi@4022 2bf07f67-1f50-48e5-81db-618ea08fc75d

--- rpm/cluster-tools/etc/cluster/scripts/cuda.sh
+++ rpm/cluster-tools/etc/cluster/scripts/cuda.sh
@@ -4,12 +4,12 @@ if [ ! -z "$NVIDIACHECK" ]; then
  ln -s `awk '{ if ($2 == "/") print $1; }' /etc/fstab` /dev/root
  yum -y install kmod-nvidia
  echo "chmod 0666 /dev/nvidia*" >> /etc/rc.d/rc.local
+ GT520CHECK=`lspci -d 10de:1040`
+  if [ -z ! "$GT520CHECK" ]; then
+  	yum -y install nvidia-cuda-lite 
+  else 
+	yum -y install nvidia-cuda
+  fi
 fi
 
-GT520CHECK=`lspci -d 10de:1040`
-if [ -z ! "$GT520CHECK" ]; then
-	yum -y install nvidia-cuda-lite 
-else 
-	yum -y install nvidia-cuda
-fi
 	
