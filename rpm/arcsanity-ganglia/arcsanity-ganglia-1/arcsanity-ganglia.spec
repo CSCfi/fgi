ca37d6c56cd5aa02e8d506fdd7289b9b54a90c49
Ulf Tigerstedt <ulf.tigerstedt@csc.fi>
2012-07-23 20:54:01 +0000

    Added a arcsanity-ganglia package, with still nonworking code. Intended to graph some important things in Ganglia from the ARC CE
    git-svn-id: svn+ssh://svn.csc.fi/svn/fgi@4811 2bf07f67-1f50-48e5-81db-618ea08fc75d

---
+++ rpm/arcsanity-ganglia/arcsanity-ganglia-1/arcsanity-ganglia.spec
@@ -0,0 +1,52 @@
+Name:		arcsanity-ganglia
+Version:	1
+Release:	1%{?dist}
+Summary:	Ganglia python scripts to do sanity checking on ARC CEs
+
+Group:		Network
+License:	GPL
+URL:		http://pulse.fgi.csc.fi/
+Source0:	arcsanity-ganglia-%{version}.tar.gz
+BuildRoot:	%(mktemp -ud %{_tmppath}/%{name}-%{version}-%{release}-XXXXXX)
+
+BuildRequires:	python
+Requires:	ganglia-gmond-python
+
+%description
+Script to do sanity checking on ARC CEs.
+- Count UR records
+- Count sessiondirs
+- Count jobstatus files
+
+%prep
+%setup -q
+
+
+%build
+#%configure
+#make %{?_smp_mflags}
+
+
+%install
+rm -rf %{buildroot}
+#make install DESTDIR=%{buildroot}
+mkdir -p %{buildroot}/etc/ganglia/conf.d
+mkdir -p %{buildroot}/usr/lib64/ganglia/python_modules/
+cp arcsanity.pyconf %{buildroot}/etc/ganglia/conf.d
+cp arcsanity.py  %{buildroot}/usr/lib64/ganglia/python_modules/
+
+%clean
+rm -rf %{buildroot}
+
+
+%files
+%defattr(-,root,root,-)
+/etc/ganglia/conf.d/*
+/usr/lib64/ganglia/python_modules/*
+%doc
+
+
+
+%changelog
+* Mon Jul 23 2012 <ulf.tigerstedt@csc.fi> 1-1
+- First version
---
+++ rpm/arcsanity-ganglia/arcsanity-ganglia-1/arcsanity.py
@@ -0,0 +1,64 @@
+
+sysdir='/sys/class/infiniband/mlx4_0/ports/1/counters/'
+
+def counter_data_handler(name):
+	# print sysdir + '/' + name
+	try: 
+		f = open(sysdir + '/' + name )
+	except IOError:
+		return 0
+
+	for l in f:
+		line = l.split()
+
+	f.close()
+	return int(line[0])
+
+def ur_count(name):
+	return 10
+
+def sesdir_count(name):
+	return 10
+
+
+def metric_init(params):
+    global descriptors, sysdir
+
+    if 'sysdir' in params:
+        sysdir = params['sysdir']
+
+    ur = {'name': 'ur_count',
+        'call_back': ur_count,
+        'time_max': 90,
+        'value_type': 'uint',
+        'units': 'bytes',
+        'slope': 'both',
+        'format': '%u',
+        'description': 'UR records unsent',
+        'groups': 'arc-sanity'}
+    ses = {'name': 'sessiondir_count',
+        'call_back': sesdir_count,
+        'time_max': 90,
+        'value_type': 'uint',
+        'units': 'bytes',
+        'slope': 'both',
+        'format': '%u',
+        'description': 'Sessiondirs present',
+        'groups': 'arc-sanity'}
+
+
+    descriptors = [ur,ses]
+
+    return descriptors
+
+def metric_cleanup():
+    '''Clean up the metric module.'''
+    pass
+
+#This code is for debugging and unit testing
+if __name__ == '__main__':
+    metric_init({})
+    for d in descriptors:
+        v = d['call_back'](d['name'])
+        print 'value for %s is %u' % (d['name'],  v)
+
